82ae9fe88602f07879b6d176df2a175ebf674034
pranavmamatha <pranavranjithkannan@gmail.com>
2026-04-21 16:38:29 +0530

    added a database to store the track the deployments

--- api/index.ts
+++ api/index.ts
@@ -1,21 +1,28 @@
 import express from "express";
+import { pool } from "./db.ts"
 const app = express();
 app.use(express.json())
 
-app.post("/deploy", (req, res) => {
+app.post("/deploy", async (req, res) => {
   try {
-    const data = req.body;
-    const repoUrl = data.repoUrl
-    const buildCommand = data.buildCommand
-    if (buildCommand && repoUrl) {
-      res.send({ status: "recived" })
-    } else {
-      res.send({ status: "did not recive" })
+    const { repoUrl, buildCommand } = req.body;
+    if (!buildCommand || !repoUrl) {
+      return res.status(400).json({ error: "missing fields" })
     }
-  } catch {
-    res.send({ status: "failed" })
+    const result = await pool.query(
+      `insert into deployments (repo_url, build_command)
+       values ($1, $2)
+       returning *`,
+      [repoUrl, buildCommand]
+
+    );
+    res.status(202).json({ status: "stored", deployment: result.rows[0] })
+
+  } catch (err) {
+    console.error(err)
+    res.status(500).json({ status: "db failed" })
   }
-})
+});
 
 app.listen(3000);
 
